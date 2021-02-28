256d4a60856e3340bbe0e643a23ffcba208ba5e6
den10004 <67386666+den10004@users.noreply.github.com>
2021-02-28 14:41:42 +0300

    Add files via upload
    update

---
+++ src/components/app/app.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ErrorBoundry from '../error-boundry';
+import SwapiService from '../../services/swapi-service';
+
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import { SwapiServiceProvider } from '../swapi-service-context';
+import { StarshipDetails } from '../sw-components';
+import './app.css';
+
+
+export default class App extends Component {
+
+  state = {
+    swapiService: new SwapiService(),
+  };
+
+  render() {
+
+    return (
+      <ErrorBoundry>
+        <SwapiServiceProvider value={this.state.swapiService} >
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
+              <Switch>
+                <Route path="/" render={() => <h2>Welcome to StarWars</h2>} exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" component={StarshipsPage} exact />
+                <Route path="/starships/:id"
+                  render={({ match }) => {
+                    const { id } = match.params;
+                    return <StarshipDetails itemId={id} />
+                  }} /> 
+                <Route  render={() => <h2>Page not found</h2>} />
+              </Switch>
+            </div>
+          </Router>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
+    );
+  }
+}
---
+++ src/components/pages/starships-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StarshipList } from '../sw-components';
+import {withRouter} from 'react-router-dom'
+
+const StarshipsPage = ({history}) => {
+    return (
+      <StarshipList onItemSelected={(itemId) => {
+        const newPath = (itemId)
+        history.push(newPath)
+      }} />
+    );
+}
+export default withRouter(StarshipsPage)
